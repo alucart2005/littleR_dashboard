e864f5596bee2087b31ac5510e4a44f1e386382a
Napoleon Anaya R <napoleonanaya@gmail.com>
2023-12-31 20:37:28 -0500

    Feature: starting

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
 import "./App.css";
 import { MyRoutes } from "./routers/routes";
+import styled from "styled-components";
 
 function App() {
-  return <MyRoutes />;
+  return (
+    <Container>
+      <MyRoutes />;
+    </Container>
+  );
 }
 
 export default App;
+const Container = styled.div`
+  display: flex;
+  height: 97vh;
+  background: blue;
+  border-radius: 2rem;
+  @media screen and (min-width: 320px) and (max-width:1080px) {
+    flex-direction: column;
+    /* background-color:green */
+  }
+`;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,17 @@
+import styled from "styled-components";
+
 export function Home() {
-  return (<h1>Hi Home</h1>);
+  return (
+    <Container>
+
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  width: 80%;
+  border-bottom-right-radius: 2rem;
+  background: red;
+  border-top-right-radius: 2rem;
+  margin: 1rem 8rem 1rem 4rem;
+`
