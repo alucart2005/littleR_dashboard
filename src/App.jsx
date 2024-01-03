489d130ee6bbcdf21edff5c5c433d58e3dec39b3
Napoleon Anaya R <napoleonanaya@gmail.com>
2024-01-03 18:54:03 -0500

    WIP: e674942 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,35 @@
-import "./App.css";
-import { MyRoutes } from "./routers/routes";
-import styled from "styled-components";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <Container>
-      <MyRoutes />;
-    </Container>
-  );
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
-const Container = styled.div`
-  display: flex;
-  height: 97vh;
-  background: blue;
-  border-radius: 2rem;
-  @media screen and (min-width: 320px) and (max-width:1080px) {
-    flex-direction: column;
-  }
-`;
+export default App
