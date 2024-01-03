4abcc00300e4baa484fc7f2591a34c5b40df38aa
Napoleon Anaya R <napoleonanaya@gmail.com>
2024-01-03 18:41:07 -0500

    Feature: using styled components

--- src/components/Ganancias.jsx
+++ src/components/Ganancias.jsx
@@ -1,3 +1,22 @@
+import styled from "styled-components";
+import { themeColor, hoverEffect } from "../utils/utils";
+
 export function Ganancias() {
-  return (<h1>Hi Ganancias</h1>);
+  return <EarningsCard></EarningsCard>;
 }
+
+const EarningsCard = styled.div`
+  height: 100%;
+  width: 14rem;
+  background-color: ${themeColor};
+  padding: 1rem;
+  border-radius: 1rem;
+  color: white;
+  transition: 0.4s ease-in-out;
+  &:hover {
+    box-shadow: ${hoverEffect};
+  }
+  @media screen and (min-width: 320px) and (max-width: 1080px) {
+    width: 80%;
+  }
+`;
---
+++ src/utils/utils.jsx
@@ -0,0 +1,3 @@
+export const themeColor = "#6100D4";
+export const hoverEffect = `rgba(0, 0, 0, 0.56) 0px 22px 70px 
+4px`;
